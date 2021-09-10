f642ccdbc1713b5d75892566295279f6f927313b
Yiang <peteryiang@gmail.com>
2021-09-10 00:12:29 -0700

    rm secret

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
-const KEY = 'AIzaSyAqK_r4C1fpWYgdYNz-CP79MVYbJlWiKp0'
-
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
@@ -9,6 +7,6 @@ export default axios.create({
         part: "snippet",
         type: 'video',
         maxResult: 5,
-        key: KEY
+        key: process.env.REACT_APP_YOUTUBE_API_KEY,
     }
 })
